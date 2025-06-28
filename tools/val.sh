01248673bf5430f6bd76447090a0502eed2c0465
ghost233lism <jin_modi@outlook.com>
2025-06-28 11:44:20 +0000

    initial submit

---
+++ tools/val.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+now=$(date +"%Y%m%d_%H%M%S")
+
+config=configs/synthetic_uncertainty_unsupervised.yaml
+torchrun \
+    --nproc_per_node=$1 \
+    --master_addr=localhost \
+    --master_port=$2 \
+    evaluate_depth.py \
+    --config=$config  \
+    --port $2 \
+    
